2414fd7c404ffe201c0fb9638b83981b6abe5baf
Ybadmus <yusif.badmus@gmail.com>
2021-02-24 15:11:37 +0000

    added select in select query

---
+++ select_within_select.sql
@@ -0,0 +1,8 @@
+-- 1. List each country name where the population is larger than that of 'Russia'. 
+
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia');
+
+-- 2. 
