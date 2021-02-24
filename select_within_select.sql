f013a76fe31b2c734ffe89eeb0d3a41fbc6b6b6c
Ybadmus <yusif.badmus@gmail.com>
2021-02-24 15:31:38 +0000

    List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country

--- select_within_select.sql
+++ select_within_select.sql
@@ -8,7 +8,14 @@ SELECT name FROM world
 -- 2. Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
 
 SELECT name
-FROM world 
+ FROM world 
 WHERE continent = 'Europe' AND gdp/population > (SELECT gdp/population
                FROM world 
                WHERE name = 'United Kingdom');
+
+-- 3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+
+SELECT name, continent 
+ FROM world 
+WHERE continent IN (SELECT continent FROM world 
+ WHERE name IN ('Argentina', 'Australia')) ORDER BY name;
