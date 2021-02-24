6234582097351651be7f0b93627971ecc6bd3d04
Ybadmus <yusif.badmus@gmail.com>
2021-02-24 12:10:33 +0000

    SELECT name

--- select_world.sql
+++ select_world.sql
@@ -1,18 +1,19 @@
 -- 1. Observe the result of running this SQL command to show the name, continent and population of all countries. 
 
-SELECT name, continent, population FROM world;
+SELECT name, continent, population 
+  FROM world;
 
 -- 2.  Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros. 
 
 SELECT name
   FROM world
- WHERE population >= 200000000
+ WHERE population >= 200000000;
 
  -- 3. Give the name and the per capita GDP for those countries with a population of at least 200 million. 
 
  SELECT name, gdp, population
   FROM world
- WHERE population >= 200000000
+ WHERE population >= 200000000;
 
 -- 4. Show the name and population in millions for the countries of the continent 'South America'.
 
@@ -24,4 +25,10 @@ SELECT name, population/1000000
 
 SELECT name, population 
  FROM world 
-WHERE name IN ('France', 'Germany', 'Italy')
+WHERE name IN ('France', 'Germany', 'Italy');
+
+-- 6. Show the countries which have a name that includes the word 'United' 
+
+SELECT name 
+ FROM world 
+WHERE name LIKE ('%United%');
