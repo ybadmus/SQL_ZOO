8233c0c3fe9f1f5b27cd17adbcf0f85dda59b50a
Ybadmus <yusif.badmus@gmail.com>
2021-02-24 12:27:38 +0000

    For South America show population in millions and GDP in billions both to 2 decimal places

--- select_world.sql
+++ select_world.sql
@@ -44,3 +44,9 @@ WHERE area > 3000000 OR population > 250000000;
 SELECT name, population, area 
  FROM world
 WHERE area > 3000000 XOR population > 250000000;
+
+-- For South America show population in millions and GDP in billions both to 2 decimal places.
+
+SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) 
+ FROM world
+WHERE continent LIKE 'South America';
