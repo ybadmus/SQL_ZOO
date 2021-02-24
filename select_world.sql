4da695015a594fae198f275db1dc5106c5690477
Ybadmus <yusif.badmus@gmail.com>
2021-02-24 12:00:33 +0000

    Give the name and the per capita GDP for those countries with a population of at least 200 million

--- select_world.sql
+++ select_world.sql
@@ -7,3 +7,9 @@ SELECT name, continent, population FROM world;
 SELECT name
   FROM world
  WHERE population >= 200000000
+
+ --3. Give the name and the per capita GDP for those countries with a population of at least 200 million. 
+
+ SELECT name, gdp, population
+  FROM world
+ WHERE population >= 200000000
