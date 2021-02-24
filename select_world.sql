f64bb511f6e9d9d57d3b330c8b007d5675874750
Ybadmus <yusif.badmus@gmail.com>
2021-02-24 12:07:53 +0000

    Show the name and population for France, Germany, Italy

--- select_world.sql
+++ select_world.sql
@@ -19,3 +19,9 @@ SELECT name
 SELECT name, population/1000000
   FROM world
  WHERE continent =  'South America';
+
+-- 5. Show the name and population for France, Germany, Italy 
+
+SELECT name, population 
+ FROM world 
+WHERE name IN ('France', 'Germany', 'Italy')
