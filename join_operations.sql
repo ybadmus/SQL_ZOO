e7271b6f307f9ccef8305815155be5aa36c18b27
ybadmus <yusif.badmus@gmail.com>
2021-02-25 22:18:07 +0000

    List the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.

--- join_operations.sql
+++ join_operations.sql
@@ -5,4 +5,13 @@ SELECT matchid, player FROM goal WHERE teamid LIKE 'GER';
 SELECT id, stadium, team1, team2 FROM game WHERE id = 1012;
 
 -- 3. Modify it to show the player, teamid, stadium and mdate for every German goal.
-SELECT player, teamid, stadium, mdate FROM game JOIN goal ON (matchid=id) WHERE teamid='GER';
+SELECT player, teamid, stadium, mdate FROM game JOIN goal ON (matchid = id) WHERE teamid='GER';
+
+-- 4. Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+SELECT team1, team2, player FROM game JOIN goal ON (id=matchid) WHERE player LIKE 'Mario%';
+
+-- 5. Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
+SELECT player, teamid,coach, gtime FROM goal JOIN eteam ON (teamid=id) WHERE gtime <= 10;
+
+-- 6. List the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
+SELECT mdate, teamname FROM game JOIN eteam ON (game.team1=eteam.id) WHERE coach = 'Fernando Santos';
--- sum_and_count.sql
+++ sum_and_count.sql
@@ -3,22 +3,35 @@ SELECT SUM(population)
  FROM world;
 
 -- 2. List all the continents - just once each.
-select distinct continent FROM world;
+SELECT DISTINCT continent 
+ FROM world;
 
 -- 3. Give the total GDP of Africa
-select SUM(gdp) FROM world WHERE continent = 'Africa'
+SELECT SUM(gdp) 
+ FROM world 
+WHERE continent = 'Africa';
 
 -- 4. How many countries have an area of at least 1000000
-select count(name) FROM world WHERE area >= 1000000;
+SELECT count(name) 
+ FROM world 
+WHERE area >= 1000000;
 
 -- 5. What is the total population of ('Estonia', 'Latvia', 'Lithuania')
-select sum(population) FROM world WHERE name in ('Estonia', 'Latvia', 'Lithuania')
+SELECT sum(population) 
+ FROM world 
+WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
 
 -- 6. For each continent show the continent and number of countries.
-select continent, count(name) FROM world group by continent;
+SELECT continent, count(name) 
+ FROM world GROUP by continent;
 
 -- 7. For each continent show the continent and number of countries with populations of at least 10 million.
-select continent, count(name) FROM world WHERE population >= 10000000 group by continent;
+SELECT continent, count(name) 
+ FROM world 
+WHERE population >= 10000000 GROUP by continent;
 
 -- 8. List the continents that have a total population of at least 100 million.
-select distinct continent FROM world x WHERE 100000000 <= (select sum(population) FROM world y WHERE x.continent = y.continent);
+SELECT DISTINCT continent 
+ FROM world x 
+WHERE 100000000 <= 
+  (SELECT sum(population) FROM world y WHERE x.continent = y.continent);
