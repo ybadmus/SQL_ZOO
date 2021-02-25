6c3087918a60ee87a4c10d5bd05d6be29a57280d
ybadmus <yusif.badmus@gmail.com>
2021-02-25 22:41:27 +0000

    Use COUNT and GROUP BY

--- using_null.sql
+++ using_null.sql
@@ -15,3 +15,9 @@ SELECT teacher.name, COALESCE(mobile, '07986 444 2266') FROM teacher;
 
 -- 6. Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
 SELECT teacher.name, COALESCE(dept.name, 'None') FROM teacher LEFT JOIN dept ON (teacher.dept = dept.id);
+
+-- 7. Use COUNT to show the number of teachers and the number of mobile phones.
+SELECT COUNT(teacher.name), COUNT(teacher.mobile) FROM teacher;
+
+-- 8. Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
+SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON (teacher.dept = dept.id) GROUP BY dept.name;
