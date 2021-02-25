e35e98585934d5c277fccd2ee2f58e3c888d64a7
ybadmus <yusif.badmus@gmail.com>
2021-02-25 21:59:40 +0000

    For each continent show the continent and number of countries

--- sum_and_count.sql
+++ sum_and_count.sql
@@ -6,3 +6,12 @@ select distinct continent from world
 
 -- 3. Give the total GDP of Africa
 select SUM(gdp) from world where continent = 'Africa'
+
+-- 4. How many countries have an area of at least 1000000
+select count(name) from world where area >= 1000000
+
+-- 5. What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+select sum(population) from world where name in ('Estonia', 'Latvia', 'Lithuania')
+
+-- 6. For each continent show the continent and number of countries.
+select continent,count(name) from world group by continent
