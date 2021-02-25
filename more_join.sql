239f49a033222891f4bb1a21c313a4249b66dcf0
ybadmus <yusif.badmus@gmail.com>
2021-02-25 22:22:41 +0000

    Obtain the cast list for 'Casablanca'

--- more_join.sql
+++ more_join.sql
@@ -7,3 +7,12 @@ SELECT yr FROM movie WHERE title = 'Citizen Kane';
 
 -- 3. List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
 SELECT id, title, yr FROM movie WHERE title LIKE 'Star Trek%' ORDER BY yr;
+
+-- 4. What id number does the actor 'Glenn Close' have?
+SELECT id FROM actor WHERE name = 'Glenn Close';
+
+-- 5. What is the id of the film 'Casablanca'
+SELECT id FROM movie WHERE title = 'Casablanca';
+
+-- 6. Obtain the cast list for 'Casablanca'.
+SELECT actor.name FROM actor JOIN casting ON casting.actorid = actor.id WHERE movieid = 11768;
