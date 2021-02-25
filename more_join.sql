2d80144a35b2cf0424dc24e0a767901c31c0789d
ybadmus <yusif.badmus@gmail.com>
2021-02-25 22:23:36 +0000

    List the films together with the leading star for all 1962 films.

--- more_join.sql
+++ more_join.sql
@@ -16,3 +16,16 @@ SELECT id FROM movie WHERE title = 'Casablanca';
 
 -- 6. Obtain the cast list for 'Casablanca'.
 SELECT actor.name FROM actor JOIN casting ON casting.actorid = actor.id WHERE movieid = 11768;
+
+-- 7. Obtain the cast list for the film 'Alien'
+SELECT actor.name FROM actor JOIN casting ON actor.id = casting.actorid JOIN movie ON movie.id = casting.movieid WHERE movie.title = 'Alien';
+
+-- 8. List the films in which 'Harrison Ford' has appeared
+SELECT movie.title FROM actor JOIN casting ON actor.id = casting.actorid JOIN movie ON movie.id = casting.movieid WHERE actor.name = 'Harrison Ford';
+
+-- 9. List the films where 'Harrison Ford' has appeared - but not in the starring role. [Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role]
+SELECT movie.title FROM actor JOIN casting ON actor.id = casting.actorid JOIN movie ON movie.id = casting.movieid WHERE actor.name = 'Harrison Ford' AND casting.ord != 1;
+
+-- 10. List the films together with the leading star for all 1962 films.
+SELECT movie.title, actor.name FROM actor JOIN casting ON actor.id = casting.actorid JOIN movie ON movie.id = casting.movieid WHERE casting.ord = 1 AND movie.yr = 1962;
+
