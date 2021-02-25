8756b39951093035a26d56dd7b1cd44091a8d640
ybadmus <yusif.badmus@gmail.com>
2021-02-25 23:17:36 +0000

    Find the routes involving two buses that can go from Craiglockhart to Lochend

--- self_join.sql
+++ self_join.sql
@@ -32,3 +32,28 @@ a JOIN route b ON (a.company = b.company AND a.num = b.num)
 JOIN stops stopa ON (a.stop = stopa.id) 
 JOIN stops stopb ON (b.stop = stopb.id) 
 WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'London Road';
+
+-- 7. Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+SELECT DISTINCT a.company, a.num FROM route a JOIN route b ON (a.company = b.company AND a.num = b.num)
+JOIN stops stopa ON (a.stop = stopa.id) JOIN stops stopb ON (b.stop = stopb.id) WHERE stopa.name = 'Haymarket' AND stopb.name = 'Leith';
+
+-- 8. Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+SELECT DISTINCT a.company, a.num FROM route a JOIN route b ON (a.company = b.company AND a.num = b.num)
+JOIN stops stopa ON (a.stop = stopa.id) JOIN stops stopb ON (b.stop = stopb.id)
+WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross';
+
+-- 9. Give a distinct list of the stops which may be reached from 'Craiglockhart' by 
+-- taking one bus, including 'Craiglockhart' itself, offered by the LRT company. 
+SELECT DISTINCT stopb.name, b.company, b.num FROM route a JOIN route b ON a.company = b.company 
+AND a.num = b.num JOIN stops stopa ON a.stop = stopa.id JOIN stops stopb ON b.stop = stopb.id
+WHERE stopa.name = 'Craiglockhart' AND a.company = 'LRT';
+
+-- 10. Find the routes involving two buses that can go from Craiglockhart to Lochend.
+SELECT a.num Num1, a.comp Comp1, a.transfer Transfer, b.num Num2, b.comp Comp2
+FROM (SELECT DISTINCT a.num num, a.company comp, stopb.name transfer 
+FROM route a JOIN route b ON a.company = b.company AND a.num = b.num
+JOIN stops stopa ON a.stop = stopa.id JOIN stops stopb ON b.stop = stopb.id
+WHERE stopa.name = 'Craiglockhart') a JOIN (SELECT DISTINCT a.num num, a.company comp, stopb.name transfer
+FROM route a JOIN route b ON a.company = b.company AND a.num = b.num JOIN stops stopa ON a.stop = stopa.id
+JOIN stops stopb ON b.stop = stopb.id WHERE stopa.name = 'Lochend') b ON a.transfer = b.transfer
+ORDER BY Num1,Transfer, Num2;
